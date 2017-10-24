49a17811fa330a5ca1bff4ac23f57087494baa78
Brendan Jackman <brendan.jackman@arm.com>
2017-10-24 13:25:56 +0100

    FROMLIST: sched/fair: Update blocked load from newly idle balance
    We now have a NOHZ kick to avoid the load of idle CPUs becoming stale. This is
    good, but it brings about CPU wakeups, which have an energy cost. As an
    alternative to waking CPUs up to do decay blocked load, we can sometimes do it
    from newly idle balance. If the newly idle balance is on a domain that covers
    all the currently nohz-idle CPUs, we push the value of nohz.next_update into the
    future. That means that if such newly idle balances happen often enough, we
    never need wake up a CPU just to update load.
    
    Since we're doing this new update inside a for_each_domain, we need to do
    something to avoid doing multiple updates on the same CPU in the same
    idle_balance. A tick stamp is set on the rq in update_blocked_averages as a
    simple way to do this. Using a simple jiffies-based timestamp, as opposed to the
    last_update_time of the root cfs_rq's sched_avg, means we can do this without
    taking the rq lock.
    
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Morten Rasmussen <morten.rasmussen@arm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Brendan Jackman <brendan.jackman@arm.com>
    Change-Id: I39423091e6bf789c1579cb431930c449a3c8239a
    [merge conflicts]
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: ederekun <sedrickvince@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8463,6 +8463,9 @@ static void update_blocked_averages(int cpu)
 	}
 
 	update_rt_rq_load_avg(rq_clock_task(rq), cpu, &rq->rt, 0);
+#ifdef CONFIG_NO_HZ_COMMON
+	rq->last_blocked_load_update_tick = jiffies;
+#endif
 	rq_unlock_irqrestore(rq, &rf);
 }
 
@@ -8523,6 +8526,9 @@ static inline void update_blocked_averages(int cpu)
 	update_rq_clock(rq);
 	update_cfs_rq_load_avg(cfs_rq_clock_task(cfs_rq), cfs_rq, true);
 	update_rt_rq_load_avg(rq_clock_task(rq), cpu, &rq->rt, 0);
+#ifdef CONFIG_NO_HZ_COMMON
+	rq->last_blocked_load_update_tick = jiffies;
+#endif
 	rq_unlock_irqrestore(rq, &rf);
 }
 
@@ -9125,6 +9131,30 @@ static inline void update_sd_lb_stats(struct lb_env *env, struct sd_lb_stats *sd
 	int load_idx;
 	bool overload = false, overutilized = false;
 	bool prefer_sibling = child && child->flags & SD_PREFER_SIBLING;
+
+#ifdef CONFIG_NO_HZ_COMMON
+	if (env->idle == CPU_NEWLY_IDLE) {
+		int cpu;
+
+		/* Update the stats of NOHZ idle CPUs in the sd */
+		for_each_cpu_and(cpu, sched_domain_span(env->sd),
+				 nohz.idle_cpus_mask) {
+			struct rq *rq = cpu_rq(cpu);
+
+			/* ... Unless we've already done since the last tick */
+			if (time_after(jiffies,
+                                       rq->last_blocked_load_update_tick))
+				update_blocked_averages(cpu);
+		}
+	}
+	/*
+	 * If we've just updated all of the NOHZ idle CPUs, then we can push
+	 * back the next nohz.next_update, which will prevent an unnecessary
+	 * wakeup for the nohz stats kick
+	 */
+	if (cpumask_subset(nohz.idle_cpus_mask, sched_domain_span(env->sd)))
+		nohz.next_update = jiffies + LOAD_AVG_PERIOD;
+#endif
 
 	load_idx = get_sd_load_idx(env->sd, env->idle);
 
@@ -10787,10 +10817,12 @@ static void run_rebalance_domains(struct softirq_action *h)
 	 */
 	nohz_idle_balance(this_rq, idle);
 	update_blocked_averages(this_rq->cpu);
+#ifdef CONFIG_NO_HZ_COMMON
 	if (!test_bit(NOHZ_STATS_KICK, nohz_flags(this_rq->cpu)))
 		rebalance_domains(this_rq, idle);
-#ifdef CONFIG_NO_HZ_COMMON
 	clear_bit(NOHZ_STATS_KICK, nohz_flags(this_rq->cpu));
+#else
+	rebalance_domains(this_rq, idle);
 #endif
 }
 
