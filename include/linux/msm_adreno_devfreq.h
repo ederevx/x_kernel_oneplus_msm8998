7d2fa5aaf494ce6cb72ef250dccd010c264ce4e8
Akhil P Oommen <akhilpo@codeaurora.org>
2018-06-07 18:39:27 +0530

    adreno_tz: Remove partner devfreq management
    Remove partner (bus) devfreq management from the adreno governor.
    Partner devfreq can be managed directly from the device driver.
    
    Change-Id: I745361c2b585e0dd91f208e09ec3001d7ecba717
    Signed-off-by: Akhil P Oommen <akhilpo@codeaurora.org>
    Add copyright header for msm_adreno_devfreq
    Signed-off-by: ederekun <sedrickvince@gmail.com>

--- include/linux/msm_adreno_devfreq.h
+++ include/linux/msm_adreno_devfreq.h
@@ -1,18 +1,23 @@
+/* Copyright (c) 2016-2019, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
 #ifndef MSM_ADRENO_DEVFREQ_H
 #define MSM_ADRENO_DEVFREQ_H
 
 #include <linux/devfreq.h>
 #include <linux/notifier.h>
 
-#define ADRENO_DEVFREQ_NOTIFY_SUBMIT	1
-#define ADRENO_DEVFREQ_NOTIFY_RETIRE	2
-#define ADRENO_DEVFREQ_NOTIFY_IDLE	3
-
 struct device;
-
-int kgsl_devfreq_add_notifier(struct device *, struct notifier_block *);
-
-int kgsl_devfreq_del_notifier(struct device *, struct notifier_block *);
 
 /* same as KGSL_MAX_PWRLEVELS */
 #define MSM_ADRENO_MAX_PWRLEVELS 10
@@ -54,12 +59,6 @@ struct devfreq_msm_adreno_tz_data {
 
 struct msm_adreno_extended_profile {
 	struct devfreq_msm_adreno_tz_data *private_data;
-	struct devfreq *bus_devfreq;
-	struct workqueue_struct *partner_wq;
-	struct work_struct partner_start_event_ws;
-	struct work_struct partner_stop_event_ws;
-	struct work_struct partner_suspend_event_ws;
-	struct work_struct partner_resume_event_ws;
 	struct devfreq_dev_profile profile;
 };
 
