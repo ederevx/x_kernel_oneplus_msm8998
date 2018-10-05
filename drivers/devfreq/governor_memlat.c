90f33dab363f6c129135fa679daa3d8d3002d95d
Saravana Kannan <skannan@codeaurora.org>
2018-10-04 17:14:24 -0700

    PM / devfreq: memlat: Increase range of ratio_ceil tunables
    Some instances of memlat governor need to use a higher value for ratio_ceil
    to get a better power/performance balance. So increase the max limit from
    10000 to 20000.
    
    Change-Id: I0c8900e574c91c4c02bbb49416da6df764aab030
    Signed-off-by: Saravana Kannan <skannan@codeaurora.org>

--- drivers/devfreq/governor_memlat.c
+++ drivers/devfreq/governor_memlat.c
@@ -316,7 +316,7 @@ static int devfreq_memlat_get_freq(struct devfreq *df,
 	return 0;
 }
 
-gov_attr(ratio_ceil, 1U, 10000U);
+gov_attr(ratio_ceil, 1U, 20000U);
 
 static struct attribute *dev_attr[] = {
 	&dev_attr_ratio_ceil.attr,
