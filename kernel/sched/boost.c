bf7b3f5c65968f6386af296f37c8a07eb74eb994
ederekun <sedrickvince@gmail.com>
2021-09-22 23:32:19 +0300

    sched: Dynamic SchedTune revert patchset
    Revert "sched: Turn on MIN_CAPACITY_CAPPING feature"
    
    This reverts commit 21af7a1f9c56bb67651186b3e85f90fd0d1780bf.
    
    Revert "sched/fair: Add bias schedtune boosted tasks sched feature"
    
    This reverts commit ea5578d4d3af2d7f75a348c7345c01c45af1d00d.
    
    Revert "sched/boost: Update functions for newer Dynamic Schedtune Boost changes"
    
    This reverts commit 049858690ed5dcb6f87d2a7a875773764bffde0a.
    
    Revert "sched/tune: Switch Dynamic Schedtune Boost to a slot-based tracking system"
    
    This reverts commit 025429e79f046c0e91ffe11eb83d2f28fe305694.
    
    Revert "sched/stune: Rename stune_boost() to do_stune_sched_boost()"
    
    This reverts commit 3962f27e52be9fb876e8c559e9efda592c9b25ae.
    
    Revert "sched/tune: Rename dynamic_boost parameter to sched_boost"
    
    This reverts commit e1f6f5824ca93cc8972566f1394a1f6fc19aaf7b.
    
    Revert "sched/tune: Track active boosts on a per-Schedtune basis"
    
    This reverts commit 8e9f4632dfb47aab37217ce4a23a2040b7d22f72.
    
    Revert "sched/tune: Reset Dynamic Schedtune Boost only if no more boosts running"
    
    This reverts commit 10407981f2ec2c23d03a5695287b37a42e744842.
    
    Revert "sched/boost: Perform SchedTune boosting when sched_boost is triggered"
    
    This reverts commit 199a2cfe43bb58b0e605ae3322981550718f0c75.
    
    Revert "sched/tune: Introduce stune_boost() function"
    
    This reverts commit 04c2e6dc156f8048484457389339ce86d360a578.
    
    Revert "sched/tune: Refactor do_stune_boost()"
    
    This reverts commit 0c27eaf055673018c50a7feea3cd5cc346b86ab5.
    
    Revert "sched/tune: Create dynamic_boost SchedTune parameter"
    
    This reverts commit 682102f88d67ee31395667a2eba450712ea89048.
    
    Revert "sched/tune: Rename dynamic_boost_write() to dynamic_boost()"
    
    This reverts commit 3e71f97c2a1abdc9e5a112e2e62b481046307d16.
    
    Revert "init: Make DYNAMIC_STUNE_BOOST depend on CGROUP_SCHEDTUNE"
    
    This reverts commit b6b8cc0cb57f0bcd2644b838cf7fc52551feb1ad.
    
    Revert "sched/tune: Add initial support for Dynamic SchedTune Boost"
    
    This reverts commit f711d258f764b811970f5a2b632e22c45f9f47d1.

--- kernel/sched/boost.c
+++ kernel/sched/boost.c
@@ -21,10 +21,6 @@
 
 unsigned int sysctl_sched_boost;
 
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
-static int boost_slot;
-#endif // CONFIG_DYNAMIC_STUNE_BOOST
-
 static bool verify_boost_params(int old_val, int new_val)
 {
 	/*
@@ -51,18 +47,6 @@ int sched_boost_handler(struct ctl_table *table, int write,
 
 	if (ret || !write)
 		goto done;
-
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
-	if (verify_boost_params(old_val, *data)) {
-		if (*data > 0)
-			do_stune_sched_boost("top-app", &boost_slot);
-		else
-			reset_stune_boost("top-app", boost_slot);
-	} else {
-		*data = old_val;
-		ret = -EINVAL;
-	}
-#endif // CONFIG_DYNAMIC_STUNE_BOOST
 
 done:
 	return ret;
