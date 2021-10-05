cec8b92e7b44818e44ec510d60b84addbf07002d
ederekun <sedrickvince@gmail.com>
2021-10-05 22:25:34 +0300

    Revert "sched/idle: Micro-optimize the idle loop"
    This reverts commit 6c97d2e0268c465974397a2e60a59bf1f2f4d258.

--- kernel/sched/idle.c
+++ kernel/sched/idle.c
@@ -213,7 +213,6 @@ DEFINE_PER_CPU(bool, cpu_dead_idle);
 static void cpu_idle_loop(void)
 {
 	while (1) {
-	int cpu = smp_processor_id();
 		/*
 		 * If the arch has a polling bit, we maintain an invariant:
 		 *
@@ -230,7 +229,7 @@ static void cpu_idle_loop(void)
 			check_pgt_cache();
 			rmb();
 
-			if (cpu_is_offline(cpu)) {
+			if (cpu_is_offline(smp_processor_id())) {
 				rcu_cpu_notify(NULL, CPU_DYING_IDLE,
 					       (void *)(long)smp_processor_id());
 				smp_mb(); /* all activity before dead. */
