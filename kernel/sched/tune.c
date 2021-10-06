bc2742c62430f9f7b5c5c9b40e87fe71b7c6630a
ederekun <sedrickvince@gmail.com>
2021-10-06 16:06:36 +0300

    sched/tune: Configure top-app and foreground values
    Disable prefer-idle and configure boost values. Only look for these two schedtune groups, it is not necessary to list them all.
    
    Signed-off-by: ederekun <sedrickvince@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -764,11 +764,8 @@ struct st_data {
 static void write_default_values(struct cgroup_subsys_state *css)
 {
 	static struct st_data st_targets[] = {
-		{ "audio-app",	0, 0 },
-		{ "background",	0, 0 },
-		{ "foreground",	0, 1 },
-		{ "rt",		0, 0 },
-		{ "top-app",	10, 1 },
+		{ "foreground",	5, 0 },
+		{ "top-app",	25, 0 },
 	};
 	int i;
 
